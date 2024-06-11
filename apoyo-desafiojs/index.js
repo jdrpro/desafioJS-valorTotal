da7ea1035da60c41d016c0d1feb697eaf1e01e29
Jorge Del Rio <jdrprofesionalgastronomico@gmail.com>
2024-06-10 22:01:49 -0400

    first commit

---
+++ apoyo-desafiojs/index.js
@@ -0,0 +1,8 @@
+const precioBase = 400000;
+let cantidad = 0;
+let totalPagar = 0;
+
+
+document.querySelector("#precio").innerHTML = precioBase;
+document.querySelector("#cantidad").innerHTML = cantidad;
+document.querySelector("#totalPagar").innerHTML = totalPagar;
